55fb3f1fea18cfa9c685d3d616a7e4d8605c408d
Sreenik Seal <sreenik.seal@gmail.com>
2020-03-28 02:35:26 +0530

    Patches a few things

---
+++ src/layer_details.hpp
@@ -0,0 +1,271 @@
+/**
+ * @file layer_details.hpp
+ * @author Sreenik Seal
+ *
+ * Implementation of a class that converts a given ann layer to string format.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#include <mlpack/core.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/layer/layer_types.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <unordered_map>
+#include <string>
+
+using namespace mlpack::ann;
+
+/**
+ * Implementation of a class that returns the string representation of the
+ * name of the given layer.
+ */
+class LayerTypeVisitor : public boost::static_visitor<std::unordered_map<std::string, double> >
+{
+ public:
+  //! Create the LayerNameVisitor object.
+  LayerTypeVisitor()
+  {
+  }
+
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(AtrousConvolution<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    values["kw"] = layer->KernelWidth();
+    values["kh"] = layer->KernelHeight();
+    values["dw"] = layer->StrideWidth();
+    values["dh"] = layer->StrideHeight();
+    values["padwl"] = layer->Padding().PadWLeft();
+    values["padwr"] = layer->Padding().PadWRight();
+    values["padht"] = layer->Padding().PadHTop();
+    values["padhb"] = layer->Padding().PadHBottom();
+    values["inputwidth"] = layer->InputWidth();
+    values["inputheight"] = layer->InputHeight();
+    values["dilationw"] = layer->DilationWidth();
+    values["dilationh"] = layer->DilationHeight();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(AlphaDropout<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["ratio"] = layer->Ratio();
+    values["alphadash"] = layer->AlphaDash();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(BatchNorm<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    // Values inaccessible, needs to be fixed
+
+    // values["eps"] = layer->eps();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(Constant<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    // values["outsize"] = layer->OutputSize();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(Convolution<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    values["kw"] = layer->KernelWidth();
+    values["kh"] = layer->KernelHeight();
+    values["dw"] = layer->StrideWidth();
+    values["dh"] = layer->StrideHeight();
+    values["padwl"] = layer->PadWLeft();
+    values["padwr"] = layer->PadWRight();
+    values["padht"] = layer->PadHTop();
+    values["padhb"] = layer->PadHBottom();
+    values["inputwidth"] = layer->InputWidth();
+    values["inputheight"] = layer->InputHeight();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(ELU<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["alpha"] = layer->Alpha();
+    values["lambda"] = layer->Lambda();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(Linear<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(LinearNoBias<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(MaxPooling<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    values["kw"] = layer->KernelWidth();
+    values["kh"] = layer->KernelHeight();
+    values["dw"] = layer->StrideWidth();
+    values["dh"] = layer->StrideHeight();
+    values["floor"] = layer->Floor();
+    values["inputwidth"] = layer->InputWidth(); // Might be removed
+    values["inputheight"] = layer->InputHeight();// Might be removed
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(MeanPooling<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    values["kw"] = layer->KernelWidth();
+    values["kh"] = layer->KernelHeight();
+    values["dw"] = layer->StrideWidth();
+    values["dh"] = layer->StrideHeight();
+    values["floor"] = layer->Floor();
+    values["inputwidth"] = layer->InputWidth(); // Might be removed
+    values["inputheight"] = layer->InputHeight();// Might be removed
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(LeakyReLU<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["alpha"] = layer->Alpha();
+    return values;
+  }
+
+  /*
+   * Return the name of the given layer of type Linear as a map.
+   * 
+   * @param Given layer of type Linear.
+   * @return The string representation of the layer.
+   */
+  std::unordered_map<std::string, double> LayerString(TransposedConvolution<>* layer) const
+  {
+    std::unordered_map<std::string, double> values;
+    values["insize"] = layer->InputSize();
+    values["outsize"] = layer->OutputSize();
+    values["kw"] = layer->KernelWidth();
+    values["kh"] = layer->KernelHeight();
+    values["dw"] = layer->StrideWidth();
+    values["dh"] = layer->StrideHeight();
+    values["padw"] = layer->PadWidth();
+    values["padh"] = layer->PadHeight();
+    values["inputwidth"] = layer->InputWidth();
+    values["inputheight"] = layer->InputHeight();
+    return values;
+  }
+
+  /*
+   * Return the name of the layer of specified type as a string.
+   * 
+   * @param Given layer of any type.
+   * @return A string declaring that the layer is unsupported.
+   */
+  template<typename T>
+  std::unordered_map<std::string, double> LayerString(T* /*layer*/) const
+  {
+    std::unordered_map<std::string, double> values;
+    return values;
+  }
+
+  //! Overload function call.
+  std::unordered_map<std::string, double> operator()(MoreTypes layer) const
+  {
+    return layer.apply_visitor(*this);
+  }
+
+  //! Overload function call.
+  template<typename LayerType>
+  std::unordered_map<std::string, double> operator()(LayerType* layer) const
+  {
+    return LayerString(layer);
+  }
+};
--- src/mlpack_to_torch.hpp
+++ src/mlpack_to_torch.hpp
@@ -20,6 +20,7 @@
 #include <mlpack/methods/ann/layer/layer.hpp>
 #include <mlpack/methods/ann/layer/base_layer.hpp>
 #include <mlpack/methods/ann/layer_names.hpp>
+#include "layer_details.hpp"
 #include <ensmallen.hpp>
 
 #include <torch/torch.h>
@@ -37,31 +38,32 @@ using namespace ann;
  * @return A sequential torch model corresponding to the given
  * mlpack model details
  */
-torch::nn::Sequential& transferLayers(std::vector<std::string>& layerTypes,
+torch::nn::Sequential transferLayers(std::vector<std::string>& layerTypes,
                                      std::vector<LayerTypes<>>& layers)
 {
-  torch::nn::Sequential model;
+  torch::nn::Sequential torchModel; // change this to a pointer
   auto layerTypeItr = layerTypes.begin();
   auto layerItr = layers.begin();
-  for (; layerTypeItr != layerTypes.end(); ++layerTypeItr, ++ layerItr)
+  for (; layerTypeItr != layerTypes.end(); ++layerTypeItr, ++layerItr)
   {
+    std::cout << "Layer start\n";
     auto layerType = *layerTypeItr;
+    std::cout << layerType << '\n';
+    unordered_map<string, double> values = boost::apply_visitor(LayerTypeVisitor(), *layerItr);
     if (layerType == "linear")
     {
-      Linear<>* layer = reinterpret_cast<Linear<>*>(&*layerItr);
-      model->push_back(torch::nn::Linear(layer->InputSize(),
-      layer->OutputSize(), true));
+      torchModel->push_back(torch::nn::Linear(torch::nn::LinearOptions(values["insize"], values["outsize"])));
     }
-    else if (layerType == "linearnobias")
-    {
-      LinearNoBias<>* layer = reinterpret_cast<LinearNoBias<>*>(&*layerItr);
-      model->push_back(torch::nn::Linear(layer->InputSize(),
-      layer->OutputSize(), false));
-    }
+    // else if (layerType == "linearnobias") // getter functions to be implemented
+    // {
+    //   LinearNoBias<>* layer = reinterpret_cast<LinearNoBias<>*>(&*layerItr);
+    //   torchModel->push_back(torch::nn::Linear(layer->InputSize(),
+    //   layer->OutputSize(), false));
+    // }
     // else if (layerType == "batchnorm")
     // {
     //     // not yet made customizable (eps and size)
-    //     // model->push_back(torch::nn::BatchNorm());
+    //     // torchModel->push_back(torch::nn::BatchNorm());
     // }
     // else if (layerType == "constant")
     // {
@@ -69,10 +71,10 @@ torch::nn::Sequential& transferLayers(std::vector<std::string>& layerTypes,
     // }
     else if (layerType == "convolution")
     {
-      Convolution<>* layer = reinterpret_cast<Convolution<>*>(&*layerItr);
-      model->push_back(torch::nn::Conv2d(layer->InputSize(),
-      layer->OutputSize(), layer->KernelDims(), layer->StrideDims(),
-      layer->PaddingDims(), 1, 1, true, 'zeros'));
+      torchModel->push_back(torch::nn::Conv2d(torch::nn::Conv2dOptions(values["insize"],
+      values["outsize"], {(int)values["kh"], (int)values["kw"]}).stride({(int)values["dh"],
+      (int)values["dw"]}).padding({(int)(values["padht"] +
+      values["padhb"]) / 2, (int)(values["padwl"] + values["padwr"]) / 2})));
     }
     else if (layerType == "dropout")
     {
@@ -80,13 +82,12 @@ torch::nn::Sequential& transferLayers(std::vector<std::string>& layerTypes,
     }
     else if (layerType == "leakyrelu")
     {
-      LeakyReLU<>* layer = reinterpret_cast<LeakyReLU<>*>(&*layerItr);
-      model->push_back(torch::nn::Functional(torch::leaky_relu,
-      layer->Alpha()));
+      torchModel->push_back(torch::nn::Functional(torch::leaky_relu,
+      values["alpha"]));
     }
     else if (layerType == "logsoftmax")
     {
-      model->push_back(torch::nn::Functional(torch::log_softmax));
+      // torchModel->push_back(torch::nn::Functional(torch::log_softmax, 1, torch::nullopt));
     }
     // else if (layerType == "transposedconvolution")
     // {
@@ -94,38 +95,41 @@ torch::nn::Sequential& transferLayers(std::vector<std::string>& layerTypes,
     // }
     else if (layerType == "elu")
     {
-      ELU<>* layer = reinterpret_cast<ELU<>*>(&*layerItr);
-      model->push_back(torch::nn::Functional(torch::elu,
-      layer->Alpha()));
+      // recheck the scale and "input scale" once
+      torchModel->push_back(torch::nn::Functional(torch::elu,
+      values["alpha"], 1, 1));
     }
     else if (layerType == "maxpooling")
     {
       MaxPooling<>* layer = reinterpret_cast<MaxPooling<>*>(&*layerItr);
-      model->push_back(torch::nn::Functional(torch::max_pool2d,
-      layer->KernelDims(), layer->StrideDims(), layer->PaddingDims(),
-      1, !layer->Floor()));
+      torchModel->push_back(torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(
+      {(int)values["kh"], (int)values["kw"]}).
+      stride({(int)values["dh"], (int)values["dw"]})));
+      //1, !layer->Floor()));
     }
     else if (layerType == "identity")
     {
-      model->push_back(torch::nn::Sequential()); // not too sure
+      // torchModel->push_back(torch::nn::Sequential()); // not too sure
     }
     else if (layerType == "prelu")
     {
-      PReLU<>* layer = reinterpret_cast<PReLU<>*>(&*layerItr);
-      model->push_back(torch::nn::Functional(torch::prelu, 1,
-      layer->Alpha())); // for single channel
+      // Compilation error
+
+      // PReLU<>* layer = reinterpret_cast<PReLU<>*>(&*layerItr);
+      // torchModel->push_back(torch::nn::Functional(torch::prelu, 1,
+      // layer->Alpha())); // for single channel
     }
     else if (layerType == "relu")
     {
-      model->push_back(torch::nn::Functional(torch::relu));
+      torchModel->push_back(torch::nn::Functional(torch::relu));
     }
     else if (layerType == "selu")
     {
-      model->push_back(torch::nn::Functional(torch::selu));
+      torchModel->push_back(torch::nn::Functional(torch::selu));
     }
     else if (layerType == "sigmoid")
     {
-      model->push_back(torch::nn::Functional(torch::sigmoid));
+      torchModel->push_back(torch::nn::Functional(torch::sigmoid));
     }
     // else if (layerType == "softsign")
     // {
@@ -133,18 +137,20 @@ torch::nn::Sequential& transferLayers(std::vector<std::string>& layerTypes,
     // }
     else if (layerType == "tanh")
     {
-      model->push_back(torch::nn::Functional(torch::tanh));
+      torchModel->push_back(torch::nn::Functional(torch::tanh));
     }
     else
     {
       Log::Fatal << "Unsupported layer: " << layerType << '\n';
     }
+    std::cout << "Layer end\n";
   }
-  return model;
+  std::cout << "Returning\n";
+  return torchModel;
 }
 
 /**
- * Transfer the weights of the mlpack model to the torch model
+ * Transfer the weights of the mlpack model to the torch model.
  * 
  * @param mlpackParams Weights of the mlpack model
  * @param torchParams Weiights of the torch model
@@ -155,33 +161,83 @@ void transferParameters(arma::mat& mlpackParams, std::vector<
   int mlpackParamItr = 0;
   for (auto& torchParam : torchParams)
   {
+
     int totalParam = 1;
-    torchParam = torchParam.view({-1, 1});
+    // torchParam = torchParam.view({-1, 1});
+
     auto origSize = torchParam.sizes();
+    torchParam = torchParam.t();
+    std::cout << origSize << '\n' << torchParam.sizes() << "\n\n";
+
+    torchParam = torchParam.view({-1, 1});
+    
     auto paramAccessor  = torchParam.accessor<float, 2>();
+
+    // std::cout << paramAccessor.sizes() << '\n';
+    std::cout << torchParam.sizes() << '\n';
+
     for (int i = 0; i < paramAccessor.size(0); ++i)
     {
+      // for (int j = 0; j < paramAccessor.size(1); ++j)
+      // {
+      //   paramAccessor[i][j] = mlpackParams(mlpackParamItr++, 0);
+      //   std::cout << "i= " << i << " j= " << j << " mlpackParamItr= " << mlpackParamItr << " value= " << paramAccessor[i][j] << '\n';
+      // }
       paramAccessor[i][0] = mlpackParams(mlpackParamItr++, 0);
+      std::cout << "i= " << i << " mlpackParamItr= " << mlpackParamItr << " value= " << paramAccessor[i][0] << '\n';
     }
+
     torchParam = torchParam.view(origSize);
   }
+
+
+
+  //   int totalParam = 1;
+  //   // torchParam = torchParam.view({-1, 1});
+
+  //   auto origSize = torchParam.sizes();
+
+  //   std::cout << origSize << "\n\n";
+
+  //   torchParam = torchParam.view({-1, 1});
+    
+  //   auto paramAccessor  = torchParam.accessor<float, 2>();
+
+  //   // std::cout << paramAccessor.sizes() << '\n';
+  //   std::cout << torchParam.sizes() << '\n';
+
+  //   for (int i = 0; i < paramAccessor.size(0); ++i)
+  //   {
+  //     // for (int j = 0; j < paramAccessor.size(1); ++j)
+  //     // {
+  //     //   paramAccessor[i][j] = mlpackParams(mlpackParamItr++, 0);
+  //     //   std::cout << "i= " << i << " j= " << j << " mlpackParamItr= " << mlpackParamItr << " value= " << paramAccessor[i][j] << '\n';
+  //     // }
+  //     paramAccessor[i][0] = mlpackParams(mlpackParamItr++, 0);
+  //     std::cout << "i= " << i << " mlpackParamItr= " << mlpackParamItr << " value= " << paramAccessor[i][0] << '\n';
+  //   }
+  //   if (origSize.size() == 1)
+  //     torchParam = torchParam.view(origSize);
+  //   else
+  //     torchParam = torchParam.reshape({origSize[1], origSize[0]}).t();
+  // }
 }
 
 /**
  * Get a torch model with the layer types, details
- * and weights corresponding to the given mlpack model
+ * and weights corresponding to the given mlpack model.
  * 
- * @param mlpackModel The mlpack model that is to be converted
+ * @param mlpackModel The mlpack model that is to be converted.
  * @return A sequential torch model corresponding to the given
- * mlpack model with the weights also transferred
+ * mlpack model with the weights also transferred.
  */
-torch::nn::Sequential& convert(FFN<>& mlpackModel)
+torch::nn::Sequential& convert(FFN<>& mlpackModel) // remove reference
 {
-  std::vector<LayerTypes<> > layers = mlpackModel.Network();
+  std::vector<LayerTypes<> > layers = mlpackModel.Model();
   std::vector<std::string> layerTypes;
   for (LayerTypes<> layer : layers)
   {
-    layerTypes.push_back(boost::apply_visitor(TestVisitor(), layer));
+    layerTypes.push_back(boost::apply_visitor(LayerNameVisitor(), layer));
   }
   torch::nn::Sequential torchModel = transferLayers(layerTypes, layers);
   transferParameters(mlpackModel.Parameters(), torchModel->parameters());
